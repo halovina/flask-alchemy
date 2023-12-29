c647e05b471d652c119e7c13d1cf7340045720db
alfin <alfin@linkaja.id>
2023-12-29 22:59:32 +0700

    test

--- application/apitoken/routes.py
+++ application/apitoken/routes.py
@@ -1,6 +1,6 @@
 from .import apitoken_blueprint
 from flask import jsonify, make_response, request
-from application.internal.enkripsi import signature_auth
+from application.internal.enkripsi import signature_auth, verified_signature_auth
 
 @apitoken_blueprint.route('/apitoken/signature-auth', methods=['POST'])
 def signature_auth_token():
@@ -14,3 +14,31 @@ def signature_auth_token():
             'signature': signature_auth(privatekey.replace(r'\n', '\n'), string_tosign)
         }), 200
     )
+    
+@apitoken_blueprint.route('/apitoken/access-token-b2b', methods=['POST'])
+def access_token_b2b():
+    xtimestamp = request.headers.get('X-TIMESTAMP')
+    xclientkey = request.headers.get('X-CLIENT-KEY')
+    string_tosign = "{}|{}".format(xclientkey, xtimestamp)
+    signature = request.headers.get('X-SIGNATURE')
+    
+    verified_signature = verified_signature_auth(string_tosign, signature)
+    
+    if verified_signature:
+        return make_response(
+            jsonify({
+                "responseMessage":"Successful",
+                "accessToken":"eyJhbGciOiJI",
+                "tokenType":"Bearer",
+                "expiresIn":"900",
+                "additionalInfo":{}
+            }), 200
+        )
+    
+    return make_response(
+        jsonify({
+            "responseMessage":"Unauthorized",
+        }), 401
+    )
+    
+    
--- application/internal/enkripsi.py
+++ application/internal/enkripsi.py
@@ -3,6 +3,7 @@ from Crypto.Hash import SHA256
 from Crypto.Signature import PKCS1_v1_5
 from Crypto.PublicKey import RSA
 import base64
+from application.internal.configkey import publicKey
 
 def signature_auth(privKey, stringToSign):
     digest = SHA256.new(bytes(stringToSign, 'utf-8'))
@@ -11,3 +12,10 @@ def signature_auth(privKey, stringToSign):
     signature_binary = PKCS1_v1_5.new(private_key).sign(digest)
     signature_binary_tobase64 = base64.b64encode(signature_binary).decode()
     return signature_binary_tobase64
+
+def verified_signature_auth(signature, stringToSign):
+    signatureDecodeB64 = base64.b64decode(signature)
+    digest = SHA256.new(bytes(stringToSign, 'utf-8'))
+    public_key = RSA.importKey(publicKey)
+    verified = PKCS1_v1_5.new(public_key).verify(digest, signatureDecodeB64)
+    return verified
